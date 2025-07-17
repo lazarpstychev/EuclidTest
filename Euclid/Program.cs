23eb2af761f036261d3ce1ffe6be83ed8d91da6d
lazarpstoychev <lazarps@abv.bg>
2025-07-17 08:24:34 +0300

    Initial commit

---
+++ Euclid/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Euclid
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            { Console.WriteLine(NOD(24,88)); }
+        }
+        public static int NOD(int a, int b)
+        {
+            
+            {
+               // a = Math.Abs(a);
+              // b = Math.Abs(b);
+                while (b != 0)
+                {
+                    int temp = b;
+                    b = a % b;
+                    a = temp;
+                }
+                return a;
+            }
+
+        }
+
+    }
+}
---
+++ EuclidTest/UnitTest1.cs
@@ -0,0 +1,33 @@
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using NUnit.Framework;
+using Euclid;
+using Program = Euclid.Program;
+
+namespace EuclidTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void CorrectResult()
+        {
+            int a = 12, b = 30;
+            int result = Program.NOD(a, b);
+            Assert.That(result, Is.EqualTo(6));
+        }
+        [Test]
+        public void NegativeDataResult()
+        {
+            int a =-12, b =30;
+            int result = Program.NOD(a, b);
+            Assert.AreEqual(result, 6);
+        }
+
+
+    }
+}
